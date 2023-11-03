7ddc0d80d0d62de29dd823843f950a750185ae02
Semih <85266757+PSEMO@users.noreply.github.com>
2023-11-03 22:46:03 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace formProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form4());
+            Application.Run(new Form3());
         }
     }
 }
